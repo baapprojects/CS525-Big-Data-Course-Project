0b3d911342334c0d3f39bbf9677d548737fb1176
Hao Zhou <hzhou@wpi.edu>
2013-10-08 15:49:38 -0400

    add comments for task D

--- Project1/MapReduceQueries/taskD.java
+++ Project1/MapReduceQueries/taskD.java
@@ -6,36 +6,40 @@ import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
+//import org.apache.hadoop.mapred.lib.MultipleInputs;
 
 public class taskD
 {
 
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text>
 	{
-		//variables to process Customer details		
+		//ID will be the key of mapper output
 		private  IntWritable ID = new IntWritable(0);
+		//Store the name attribute from mypage.txt
 		private String name;
 
 		public void map(LongWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter)throws IOException
 		{
+			//get value from input file, default: one line one record
 			String line = value.toString();
+			//divide the value by ",", so that we can detailed attributes later
 			String[] splits = line.split(",");
+
 			//Get FileName from reporter
 			FileSplit fileSplit = (FileSplit)reporter.getInputSplit();
 			String filename = fileSplit.getPath().getName();
 
 			if(filename.contains("mypage"))
 			{
-				//Deal with mypage.txt, get ID and name
-				ID.set(Integer.parseInt(splits[0]));
-				name = splits[1];
-				output.collect(ID,new Text(name));
+				//Deal with data from mypage.txt, get ID and name
+				ID.set(Integer.parseInt(splits[0])); // get ID
+				name = splits[1];					 // get name
+				output.collect(ID,new Text(name));   // output them
 			}
 			else
 			{
-				//Deal with friend.txt, get MyFriend is fine
-				ID.set(Integer.parseInt(splits[2]));
+				//Deal with data from friend.txt, get myFriend attribute only
+				ID.set(Integer.parseInt(splits[2]));  //get myFriend 
 				output.collect(ID,new Text("1"));
 			}
 			
@@ -52,15 +56,16 @@ public class taskD
 			{
 				String line = values.next().toString();
 
-				//if data from mypage.txt
+				//if data from mypage.txt, then we get the name attribute (as the lenght of name is no shorter than 10)
 				if(line.length() > 5)
 				{
 					name = line;
-					output.collect(key,new Text(name));	//This line cannot be moved outside while block, and I don't know why
+					output.collect(key,new Text(name));	//This line cannot be moved outside while block
 				}
 				else
 				{
-					countToBeFriendOfOthers++;//= countToBeFriendOfOthers + Integer.parseInt(line);
+					//if records are from myFriend.txt, just add the countToBeFriendOfOthers by 1
+					countToBeFriendOfOthers++;
 				}
 			}
 			output.collect(key,new Text(String.valueOf(countToBeFriendOfOthers)));	
@@ -98,18 +103,23 @@ public class taskD
 		JobConf conf = new JobConf(taskD.class);
 		conf.setJobName("taskD");
 
+		//set the output key/value type of mapper (if combiner defined, this should be type for combiner)
 		conf.setOutputKeyClass(IntWritable.class);
 		conf.setOutputValueClass(Text.class);
 
+		//assign map/combiner/reducer with specified classes
 		conf.setMapperClass(Map.class);
 		conf.setCombinerClass(Combiner.class);
 		conf.setReducerClass(Reduce.class);
 
+		//use default input/output format
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
 
+		//set input files path
 		FileInputFormat.addInputPath(conf, new Path("/hzhou/smallInput/mypage.txt"));
 		FileInputFormat.addInputPath(conf, new Path("/hzhou/smallInput/friends.txt"));
+		//set output file path
 		FileOutputFormat.setOutputPath(conf, new Path(args[0]));
 
 		JobClient.runJob(conf);
