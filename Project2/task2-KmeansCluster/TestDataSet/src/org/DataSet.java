8102f1f36de899ec924cdc11530a10adbcf58570
Hao Zhou <hzhou@wpi.edu>
2013-11-08 13:53:13 -0500

    re-organize the code of dataset

--- Project2/task2-KmeansCluster/TestDataSet/src/org/DataSet.java
+++ Project2/task2-KmeansCluster/TestDataSet/src/org/DataSet.java
@@ -5,45 +5,49 @@ import java.util.Random;
 
 public class DataSet 
 {
-	 private static void CreateDataSet() 
-     {
-             int count=1;
-             float x;
-             float y;
-             String lineRecordString;
-             try 
-             {
-            	 	
-                    // generate the centroids
-            	 	FileWriter fw1 = new FileWriter("initK"); 
-                    while(count<=10)
-                    {                                                
-                           x=new Random().nextFloat()*10000;
-                           y=new Random().nextFloat()*10000;
-                           lineRecordString=String.valueOf(x)+","+String.valueOf(y)+"\r\n";        
-                           fw1.write(lineRecordString);                                                        
-                           count++;
-                    }
-                    fw1.close(); 
-                    
-                    // generate point for KmeansCluster
-                    count=1;
-                    FileWriter fw2 = new FileWriter("kmeans"); 
-                    while(count<=6000000)
-                    {                                                
-                           x=new Random().nextFloat()*10000;
-                           y=new Random().nextFloat()*10000;
-                           lineRecordString=String.valueOf(x)+","+String.valueOf(y)+"\r\n";        
-                           fw2.write(lineRecordString);                                                        
-                           count++;
-                    }
-                    fw2.close(); 
-              } 
-              catch (Exception e) 
-              { 
-            	  // do nothing here
-              } 
-     }
+	private static void CreateDataSet() 
+	{
+		int count=1;
+		float x;
+		float y;
+		String lineRecordString;
+		try 
+		{
+			 	
+			// generate the centroids
+			FileWriter fwCentroids = new FileWriter("initK"); 
+			while(count<=10)
+			{
+				x=new Random().nextFloat()*10000;
+				y=new Random().nextFloat()*10000;
+				lineRecordString=String.valueOf(x)+","+String.valueOf(y)+"\r\n";	 
+				fwCentroids.write(lineRecordString);
+				count++;
+			}
+			fwCentroids.close(); 
+			
+			// generate point for KmeansCluster
+			count=1;
+			FileWriter fwTestPoints = new FileWriter("kmeans"); 
+
+			// Make 6000000 smaller when you do the test, as it is time-consuming, when the dataSet is very large
+			// The first step it to make your algorithm work, it doesn't hurt with small dataSet
+
+			while(count<=6000000)
+			{
+				x=new Random().nextFloat()*10000;
+				y=new Random().nextFloat()*10000;
+				lineRecordString=String.valueOf(x)+","+String.valueOf(y)+"\r\n";
+				fwTestPoints.write(lineRecordString);
+				count++;
+			}
+			fwTestPoints.close(); 
+		} 
+		catch (Exception e) 
+		{ 
+			// do nothing here
+		} 
+	}
 	public static void main(String[] args) 
 	{
 		CreateDataSet();
