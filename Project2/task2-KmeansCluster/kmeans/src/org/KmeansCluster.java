2ab9198e33492df09cd0d5e62e8e8ac93205427a
Hao Zhou <hzhou@wpi.edu>
2013-11-08 14:10:43 -0500

    update kmeansCluster algorithm

--- Project2/task2-KmeansCluster/kmeans/src/org/KmeansCluster.java
+++ Project2/task2-KmeansCluster/kmeans/src/org/KmeansCluster.java
@@ -23,61 +23,70 @@ import org.apache.hadoop.util.*;
 
 public class KmeansCluster extends Configured implements Tool
 {
-    private static final int MAXITERATIONS = 6;
-    private static final double THRESHOLD = 70;
+	// iteration Control, Maximun 6 iterations here
+	private static final int MAXITERATIONS = 6;
+	// threshold to determine whether a new centroid is changed from previous one
+	private static final double THRESHOLD = 70;
 
 
-    public static boolean stopIteration(Configuration conf) throws IOException //called in main
-    {
-        FileSystem fs = FileSystem.get(conf);
-        Path pervCenterFile = new Path("/hzhou/input/initK");
-        Path currentCenterFile = new Path("/hzhou/output/newCentroid/part-r-00000");
-        if(!(fs.exists(pervCenterFile) && fs.exists(currentCenterFile)))
-        {
-            System.exit(1);
-        }
-        
-        fs.delete(pervCenterFile,true);
-        if(fs.rename(currentCenterFile, pervCenterFile) == false)
-        {
-            System.exit(1);
-        }
-        
-        //check whether the centers have changed or not to determine to do iteration or not
-        boolean stop=true;
-        String line;
- 
-        FSDataInputStream in = fs.open(pervCenterFile);
-        InputStreamReader isr = new InputStreamReader(in);
-        BufferedReader br = new BufferedReader(isr);
+	public static boolean stopIteration(Configuration conf) throws IOException 
+	{
+		FileSystem fs = FileSystem.get(conf);
+		Path pervCenterFile = new Path("/hzhou/input/initK");
+		Path currentCenterFile = new Path("/hzhou/output/newCentroid/part-r-00000");
+		if(!(fs.exists(pervCenterFile) && fs.exists(currentCenterFile)))
+		{
+			System.exit(1);
+		}
+		
+		// delete pervCenterFile, then rename the currentCenterFile to pervCenterFile
+		/* Why rename needed here: 
+		 * as each iteration, the mapper will get centroid from Path("/hzhou/input/initK")
+		 * but reducer will output new centroids into Path("/hzhou/output/newCentroid/part-r-00000")
+		 */ 
+		fs.delete(pervCenterFile,true);
+		if(fs.rename(currentCenterFile, pervCenterFile) == false)
+		{
+			System.exit(1);
+		}
+		
+		//check whether the centers have changed or not to determine to do iteration or not
+		boolean stop=true;
+		String line;
+		FSDataInputStream in = fs.open(pervCenterFile);
+		InputStreamReader isr = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(isr);
 
-        while((line = br.readLine()) != null)
-        {
-            String []str1 = line.split(",");
-            int isntChange = Integer.parseInt(str1[2].trim());
-            if(isntChange < 1)
-            {
-                stop = false;
-                break;
-            }
+		/* get each line, check the third parameter, if it is 0, it means this centoid is changed from last iteration, 
+		 * so, we need more iteration
+		 */
+		while((line = br.readLine()) != null)
+		{
+			String []str1 = line.split(",");
+			int isntChange = Integer.parseInt(str1[2].trim());
+			if(isntChange < 1)
+			{
+				stop = false;
+				break;
+			}
+		}
+		
+		return stop;
+	}
+	 
+	
+	public static class ClusterMapper extends Mapper<LongWritable, Text, Text, Text>  //output<centroid,point>
+	{
+		Vector<Point> centers = new Vector<Point>();
+		Point point = new Point();
+		int k = 0;
 
-        }
-        
-        return stop;
-    }
-     
-    
-    public static class ClusterMapper extends Mapper<LongWritable, Text, Text, Text>  //output<centroid,point>
-    {
-        Vector<Point> centers = new Vector<Point>();
-        Point point = new Point();
-        int k = 0;
-        @Override
-        //clear centers
-        public void setup(Context context)
-        {
-            try
-            {
+		// load centroids from Path("/hzhou/input/initK") in setup() function
+		@Override
+		public void setup(Context context)
+		{
+			try
+			{
 				Path[] caches=DistributedCache.getLocalCacheFiles(context.getConfiguration());
 				if(caches == null || caches.length <= 0)
 				{
@@ -96,189 +105,195 @@ public class KmeansCluster extends Configured implements Tool
 						point.arr[i] = Double.parseDouble(str[i]);
 					}
 					centers.add(point);
-					k++;           
+					k++;		   
 				}
 				br.close();
-            }
+			}
 			catch(Exception e){}
-        }
-        @Override
-        //output<centroid,point>
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
-        {
+		}
+
+		@Override
+		//output<centroid,point>
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
+		{
 			
 			int index = -1;
 			double minDist = Double.MAX_VALUE;
-            String[] str = value.toString().split(",");
-            for(int i = 0;i < Point.DIMENTION; i++)
-            {
-                point.arr[i] = Double.parseDouble(str[i]);
+			String[] str = value.toString().split(",");
+
+			// load data into a point variable
+			for(int i = 0;i < Point.DIMENTION; i++)
+			{
+				point.arr[i] = Double.parseDouble(str[i]);
 			}
 
-            for(int i = 0;i < k; i++)
-            {
-                double dist = Point.getManhtDist(point, centers.get(i));
-                if(dist < minDist)
-                {
-                    minDist = dist;
-                    index = i;
-                }
-            }
-            context.write(new Text(centers.get(index).toString()), new Text(point.toString()));
-        }
-        
-        @Override
-        public void cleanup(Context context) throws IOException,InterruptedException 
-        {
-
-        }
-    }
-     
-    public static class Combiner extends Reducer<Text, Text, Text, Text> //value=Point_Sum+count
-    {      
-        @Override
-        //update every centroid except the last one
-        public void reduce(Text key,Iterable<Text> values,Context context) throws IOException,InterruptedException
-        {
+			// find the nearest centroid to this point
+			for(int i = 0;i < k; i++)
+			{
+				double dist = Point.getManhtDist(point, centers.get(i));
+				if(dist < minDist)
+				{
+					minDist = dist;
+					index = i;
+				}
+			}
+			// output the nearest centroid as key, and the piont as value
+			context.write(new Text(centers.get(index).toString()), new Text(point.toString()));
+		}
+		
+		@Override
+		public void cleanup(Context context) throws IOException,InterruptedException 
+		{
+			// do nothing here
+		}
+	}
+	
+	// do aggregation in local side
+	public static class Combiner extends Reducer<Text, Text, Text, Text> 
+	{	  
+		@Override
+		//update every centroid except the last one
+		public void reduce(Text key,Iterable<Text> values,Context context) throws IOException,InterruptedException
+		{
 			Point sumPoint = new Point();
 			String outputValue;
 			int count=0;
-            while(values.iterator().hasNext())
-            {
+			while(values.iterator().hasNext())
+			{
 				String line = values.iterator().next().toString();
-                String[] str1 = line.split(":");
-                
-                if(str1.length == 2)
-                {
+				String[] str1 = line.split(":");
+				/*
+				if(str1.length == 2)
+				{
 					count += Integer.parseInt(str1[1]);
 				}
-				
-                String[] str = str1[0].split(",");
-                for(int i = 0;i < Point.DIMENTION;i ++)
-                {
-                    sumPoint.arr[i] += Double.parseDouble(str[i]);
+				*/
+				String[] str = str1[0].split(",");
+				for(int i = 0;i < Point.DIMENTION;i ++)
+				{
+					sumPoint.arr[i] += Double.parseDouble(str[i]);
 				}
-                count++;
-            }
-			outputValue = sumPoint.toString() + ":" + String.valueOf(count);
-            context.write(key, new Text(outputValue));
-        }
-    }
+				count++;
+			}
+			outputValue = sumPoint.toString() + ":" + String.valueOf(count);  //value=Point_Sum+count
+			context.write(key, new Text(outputValue));
+		}
+	}
  
 	public static class UpdateCenterReducer extends Reducer<Text, Text, Text, Text> 
-    {
+	{
 		@Override
-        public void setup(Context context)
-        {
-
-        }
+		public void setup(Context context)
+		{
+			// do nothing here
+		}
  
-        @Override
-        public void reduce(Text key,Iterable<Text> values,Context context) throws IOException,InterruptedException
-        {
+		@Override
+		public void reduce(Text key,Iterable<Text> values,Context context) throws IOException,InterruptedException
+		{
 			int count = 0;
 			Point sumPoint = new Point();
 			Point newCenterPoint = new Point();
 			//String outputKey;
-            while(values.iterator().hasNext())
-            {
-                String line = values.iterator().next().toString();
-                String[] str = line.split(":");
-                String[] pointStr = str[0].split(",");
-                count += Integer.parseInt(str[1]);
-                for(int i = 0;i < Point.DIMENTION; i++)
-                {
-                    sumPoint.arr[i] += Double.parseDouble(pointStr[i]);
+			while(values.iterator().hasNext())
+			{
+				String line = values.iterator().next().toString();
+				String[] str = line.split(":");
+				String[] pointStr = str[0].split(",");
+				count += Integer.parseInt(str[1]);
+				for(int i = 0;i < Point.DIMENTION; i++)
+				{
+					sumPoint.arr[i] += Double.parseDouble(pointStr[i]);
 				}
-            }
-            for(int i = 0; i < Point.DIMENTION; i++)
-            {
-                newCenterPoint.arr[i] = sumPoint.arr[i]/count;
 			}
-            
+			for(int i = 0; i < Point.DIMENTION; i++)
+			{
+				newCenterPoint.arr[i] = sumPoint.arr[i]/count;
+			}
+			
 			String[] str = key.toString().split(",");
 			Point preCentroid = new Point();
 			for(int i = 0; i < Point.DIMENTION; i++)
-            {
+			{
 				preCentroid.arr[i] = Double.parseDouble(str[i]);
 			}
 			if(Point.getManhtDist(preCentroid, newCenterPoint) <= THRESHOLD) // compare old and new centroids
-            {
+			{
 				context.write(new Text(newCenterPoint.toString()),new Text(",1"));
 			}
 			else
 			{
 				context.write(new Text(newCenterPoint.toString()),new Text(",0"));
 			}
-        }
-        
-        @Override
-        public void cleanup(Context context) throws IOException,InterruptedException 
-        {
-
-        }
-    }
-    @Override
-    public int run(String[] args) throws Exception 
-    {
-        Configuration conf = getConf();
-        FileSystem fs = FileSystem.get(conf);
-        Job job = new Job(conf);
-        job.setJarByClass(KmeansCluster.class);
-        
-        FileInputFormat.setInputPaths(job, "/hzhou/input/kmeans");
-        Path outDir = new Path("/hzhou/output/newCentroid");
-        fs.delete(outDir,true);
-        FileOutputFormat.setOutputPath(job, outDir);
-         
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        job.setMapperClass(ClusterMapper.class);
-        job.setCombinerClass(Combiner.class);
-        job.setReducerClass(UpdateCenterReducer.class);
-        job.setNumReduceTasks(1);//
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-         
-        return job.waitForCompletion(true)?0:1;
-    }
-    
-    
-    public static void main(String[] args) throws Exception 
-    {
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(conf);
-         
-        Path dataFile = new Path("/hzhou/input/initK");
-        DistributedCache.addCacheFile(dataFile.toUri(), conf);
+		}
+		
+		@Override
+		public void cleanup(Context context) throws IOException,InterruptedException 
+		{
+			// do nothing here
+		}
+	}
+	@Override
+	public int run(String[] args) throws Exception 
+	{
+		Configuration conf = getConf();
+		FileSystem fs = FileSystem.get(conf);
+		Job job = new Job(conf);
+		job.setJarByClass(KmeansCluster.class);
+		
+		FileInputFormat.setInputPaths(job, "/hzhou/input/kmeans");
+		Path outDir = new Path("/hzhou/output/newCentroid");
+		fs.delete(outDir,true);
+		FileOutputFormat.setOutputPath(job, outDir);
+		 
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(ClusterMapper.class);
+		job.setCombinerClass(Combiner.class);
+		job.setReducerClass(UpdateCenterReducer.class);
+		job.setNumReduceTasks(1);//
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		 
+		return job.waitForCompletion(true)?0:1;
+	}
+	
+	
+	public static void main(String[] args) throws Exception 
+	{
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		 
+		Path dataFile = new Path("/hzhou/input/initK");
+		DistributedCache.addCacheFile(dataFile.toUri(), conf);
  
-        int iteration = 1;
-        int success = 1;
-        do 
-        {
-            success ^= ToolRunner.run(conf, new KmeansCluster(), args);
-            iteration++;
-        } while (success == 1 && (!stopIteration(conf)) && iteration < MAXITERATIONS );
-         
+		int iteration = 1;
+		int success = 1;
+		do 
+		{
+			success ^= ToolRunner.run(conf, new KmeansCluster(), args);
+			iteration++;
+		} while (success == 1 && (!stopIteration(conf)) && iteration < MAXITERATIONS );
+		 
 		
 		// for final output(just a mapper only task)
 		
-        Job job = new Job(conf);
-        job.setJarByClass(KmeansCluster.class);
-        
-        FileInputFormat.setInputPaths(job, "/hzhou/input/kmeans");
-        Path outDir = new Path("/hzhou/output/final");
-        fs.delete(outDir,true);
-        FileOutputFormat.setOutputPath(job, outDir);
-         
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        job.setMapperClass(ClusterMapper.class);
-        job.setNumReduceTasks(0);
-        job.setOutputKeyClass(Point.class);
-        job.setOutputValueClass(Point.class);
-         
-        job.waitForCompletion(true);
-        
-    }
+		Job job = new Job(conf);
+		job.setJarByClass(KmeansCluster.class);
+		
+		FileInputFormat.setInputPaths(job, "/hzhou/input/kmeans");
+		Path outDir = new Path("/hzhou/output/final");
+		fs.delete(outDir,true);
+		FileOutputFormat.setOutputPath(job, outDir);
+		 
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(ClusterMapper.class);
+		job.setNumReduceTasks(0);
+		job.setOutputKeyClass(Point.class);
+		job.setOutputValueClass(Point.class);
+		 
+		job.waitForCompletion(true);
+		
+	}
 }
